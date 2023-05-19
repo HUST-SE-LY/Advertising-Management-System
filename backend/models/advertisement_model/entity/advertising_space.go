a4f6dd59d23c9021b758a87e7b3ca3602a47039a
eternal <fnfunfunc@gmail.com>
2023-05-19 15:25:39 +0800

    add IsFreeIn

--- backend/models/advertisement_model/entity/advertising_space.go
+++ backend/models/advertisement_model/entity/advertising_space.go
@@ -7,3 +7,13 @@ type AdvertisingSpace struct {
 	// Indicates when the ad space will be displayed
 	DisplayTimes []advertisement_model.DisplayTime `json:"display_times" gorm:"column:display_times"`
 }
+
+// IsFreeIn : 用于判断 AdvertisingSpace 在给定的时间范围内是否空闲
+func (a *AdvertisingSpace) IsFreeIn(displayTime advertisement_model.DisplayTime) bool {
+	for _, dt := range a.DisplayTimes {
+		if (dt.StartDate < displayTime.StartDate && dt.EndDate > displayTime.StartDate) || (displayTime.StartDate < dt.StartDate && displayTime.EndDate > dt.StartDate) {
+			return false
+		}
+	}
+	return true
+}
