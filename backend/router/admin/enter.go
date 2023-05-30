2139a3af2db6c82e2ff9ef7cee15f26e0b18cf87
eternal <fnfunfunc@gmail.com>
2023-05-30 16:45:05 +0800

    chor

--- backend/router/admin/enter.go
+++ backend/router/admin/enter.go
@@ -2,6 +2,7 @@ package admin
 
 import (
 	"backend/api"
+	"backend/middleware"
 	"github.com/gin-gonic/gin"
 )
 
@@ -10,9 +11,10 @@ type AdminRouterGroup struct {
 
 func (a *AdminRouterGroup) Init(router *gin.RouterGroup) {
 	adminRouter := router.Group("admin") // Without jwt check
+	adminRouterWithJwt := router.Group("admin").Use(middleware.CompanyJwtAuth())
 	var adminAccountApi = api.ApiGroupApp.AdminApiGroup.AdminAccountApi
 	{
 		adminRouter.POST("login", adminAccountApi.AdminLogin)
-		adminRouter.GET("check_login", adminAccountApi.CheckLogin)
+		adminRouterWithJwt.GET("check_login", adminAccountApi.CheckLogin)
 	}
 }
